eac1170856f26d3728dec950d35230c5dff1e7c0
María Pía Trebucq <mariapiatrebucq@MacBook-Air-de-Maria.local>
2023-06-19 15:28:57 -0300

    agregue rutas en app.js, agregue archivos para el detalle del producto vinculé style sheet y views en carrito y detalle del producto

--- app.js
+++ app.js
@@ -3,17 +3,10 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(path.join(__dirname,"./public")));
-app.listen(3000, ()=>{
-    console.log("Servidor Corriendo")
-});
+app.listen(3000, ()=>{console.log("Servidor Corriendo")});
 
 /* aca van las rutas */
 app.get("/producto", (req,res)=>res.sendFile(path.join(__dirname, "/views/detalle-producto.html")));
-
-app.get("/login",(req, res)=>{
-    res.sendFile(path.join(__dirname + "/views/login.html"))
-  })
-
-app.get('/register', (req, res) => {
-     res.sendFile(path.join(__dirname, '/views/register.html'));
-  });
+app.get("/login",(req, res)=>{res.sendFile(path.join(__dirname + "/views/login.html"))})
+app.get("/carrito", (req,res)=>res.sendFile(path.join(__dirname, "/views/carrito.html")))
+app.get('/register', (req, res) => {res.sendFile(path.join(__dirname, '/views/register.html'));});

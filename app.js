82375c1a2922b45b067d6e9d787e626e81fda422
Nitmacc <marmacchiavelli@gmail.com>
2023-06-19 19:17:23 -0300

    Correccoin del app js

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
-app.use(express.static(path.join(__dirname,"./public")));
-app.listen(3000, ()=>{console.log("Servidor Corriendo")});
+const app = express();
+
+app.use(express.static(path.join(__dirname,'/public')));
 
 /* aca van las rutas */
-app.get("/producto", (req,res)=>res.sendFile(path.join(__dirname, "/views/detalle-producto.html")));
-app.get("/login",(req, res)=>{res.sendFile(path.join(__dirname + "/views/login.html"))})
-app.get("/carrito", (req,res)=>res.sendFile(path.join(__dirname, "/views/carrito.html")))
-app.get('/register', (req, res) => {res.sendFile(path.join(__dirname, '/views/register.html'));});
+app.get('/producto', (req,res)=>{
+    res.sendFile(path.join(__dirname + '/views/detalle-producto.html'));
+});
+app.get('/login',(req, res)=>{
+    res.sendFile(path.join(__dirname + '/views/login.html'));
+});
+app.get('/carrito', (req,res)=>{
+    res.sendFile(path.join(__dirname + '/views/carrito.html'));
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/register.html'));
+});
+
+/*server run*/
+app.listen(3000, function(){
+    console.log("Servidro Corriendo")
+});

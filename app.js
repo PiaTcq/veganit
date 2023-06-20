2c90a9e012b831d52427027452ae48f9dfa4b73c
Nitmacc <marmacchiavelli@gmail.com>
2023-06-19 23:56:51 -0300

    Home en construccion

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.static(path.join(__dirname,'/public')));
 
 /* aca van las rutas */
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
 app.get('/producto', (req,res)=>{
     res.sendFile(path.join(__dirname + '/views/detalle-producto.html'));
 });
@@ -19,7 +23,12 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/register.html'));
 });
 
+app.get('*', (req, res) => {
+    res.send("Ruta restringida no corresponde a ninguna direccion");
+});
+
 /*server run*/
+
 app.listen(3000, function(){
     console.log("Servidro Corriendo")
 });
